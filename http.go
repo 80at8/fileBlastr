d9fbab1ea04d8352dcd136ccae85a61ca2bcbae5
80at8 <48815854+80at8@users.noreply.github.com>
2020-02-27 13:26:58 -0700

    Update http.go

--- http.go
+++ http.go
@@ -65,7 +65,6 @@ func httpServer() {
 	srv := &http.Server{
 		Handler: r,
 		Addr:    "127.0.0.1:5050",
-		// Good practice: enforce timeouts for servers you create!
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
